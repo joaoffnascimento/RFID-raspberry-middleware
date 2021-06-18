f1a4c76e12ceae2a7f7e4e310cf180a62d80ede2
joaoffnascimento <thecurrentuser@live.com>
2021-06-18 09:09:32 -0300

    feat: add simple load data test to dynamoDb aws

---
+++ src/tagRegister.py
@@ -0,0 +1,19 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+table = dynamodb.Table('tags')
+
+tags = {
+    '2171081757193': 'joao',
+    '2491142008522': 'felipe',
+}
+
+for Id, User in tags.items():
+    table.put_item(
+        Item={
+            'id': Id,
+            'user': User
+        }
+    )
+    print(Id, ': ', User)
